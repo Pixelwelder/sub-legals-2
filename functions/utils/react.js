9c4a07270772bed8bc673c7edfae1c4e7ca4ce9b
Zachary Jordan <pixelwelders@gmail.com>
2021-05-05 09:17:27 -0500

    Optimize a bit.

--- functions/utils/react.js
+++ functions/utils/react.js
@@ -1,8 +1,8 @@
 module.exports = (message) => {
   const messageStr = message.content.toLowerCase();
 
-  if (message.content.toLowerCase().includes('mouth bone')) message.react('🦷');
-  if (message.content.toLowerCase().includes('human')) message.react('<:human:821874570448601118>');
-  if (message.content.toLowerCase().includes('observer')) message.react('⚠️');
-  if (message.content.toLowerCase().includes('sarya')) message.react('835217625289064458');
+  if (messageStr.includes('mouth bone')) message.react('🦷');
+  if (messageStr.includes('human')) message.react('<:human:821874570448601118>');
+  if (messageStr.includes('observer')) message.react('⚠️');
+  if (messageStr.includes('sarya')) message.react('835217625289064458');
 };
