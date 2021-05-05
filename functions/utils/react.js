0b42f877a5474f7615c4fc4a07a5ed42772c4b40
Zachary Jordan <pixelwelders@gmail.com>
2021-05-05 09:16:38 -0500

    Refactor a bit, look out for Observer.

--- functions/utils/react.js
+++ functions/utils/react.js
@@ -2,6 +2,7 @@ module.exports = (message) => {
   const messageStr = message.content.toLowerCase();
 
   if (message.content.toLowerCase().includes('mouth bone')) message.react('🦷');
-  if (message.content.toLowerCase().includes('human')) message.react('⚠️');
-  if (message.content.toLowerCase().includes('sarya')) message.react('835217625289064458'); // Sarya
+  if (message.content.toLowerCase().includes('human')) message.react('<:human:821874570448601118>');
+  if (message.content.toLowerCase().includes('observer')) message.react('⚠️');
+  if (message.content.toLowerCase().includes('sarya')) message.react('835217625289064458');
 };
