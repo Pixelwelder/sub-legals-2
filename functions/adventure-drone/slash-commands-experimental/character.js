920bca1d5ebdbe42b1e4eba2784491eccec6943a
Zack Jordan <pixelwelders@gmail.com>
2022-02-11 17:34:59 -0600

    Character basically functional.

--- functions/adventure-drone/slash-commands-experimental/character.js
+++ functions/adventure-drone/slash-commands-experimental/character.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 const { getFirestore } = require('firebase-admin/firestore');
+const { Character } = require('@pixelwelders/tlh-universe-data');
 
 // Create an array of emojis, one for each letter.
 const emojis = [
@@ -20,45 +21,194 @@ const getBar = (num, max, full = fullEmoji) => {
   return `${full.repeat(num)}${empty.repeat(max - num)}`;
 };
 
+const showCharacter = async (interaction, { ephemeral = true } = {}) => {
+  const avatarUrl = interaction.user.avatarURL({ format: 'png', dynamic: true, size: 1024 });
+  
+  const characterDocs = await getFirestore().collection('discord_characters').where('player', '==', interaction.member.id).get();
+  if (characterDocs.empty) {
+    return interaction.editReply(`You don't have a character yet. Use \`/character create\` to create one.`);
+  }
+
+  const character = characterDocs.docs[0].data();
+  console.log(`found ${characterDocs.size} character`);
+
+  const embed = new MessageEmbed()
+    .setColor('0x000000')
+    .setTitle(character.displayName)
+    .setImage(avatarUrl);
+
+  const fields = character.stats.map((stat, index) => {
+    return {
+      name: stat.displayName,
+      value: getBar(stat.value, stat.max),
+      inline: true
+    };
+  });
+
+  // fields.push({ name: 'Available points', value: getBar(character.statPoints, character.statPoints, point) });
+  embed.setFields(fields);
+
+  const message = await interaction.editReply({ embeds: [embed] });
+};
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('character')
     .setDescription(`Do stuff with your character.`)
     .addSubcommand(subcommand => subcommand
+      .setName('create')
+      .setDescription('Create your character (private).')
+    )
+    .addSubcommand(subcommand => subcommand
       .setName('examine')
-      .setDescription('Examine your character.')
+      .setDescription('Examine your character (private).')
+    )
+    .addSubcommand(subcommand => subcommand
+      .setName('show')
+      .setDescription('Show your character to the room (public).')
+    )
+    .addSubcommand(subcommand => subcommand
+      .setName('kill')
+      .setDescription('Kill your character (private).')
     ),
     
   // TODO Don't use reply.
-  async execute(interaction) {
+  async execute(interaction, character) {
+    // This function takes a number and randomly divides it into the specified number of numbers.
+    const split = (num, numParts) => {
+      // Create an array of zeroes.
+      const parts = new Array(numParts).fill(0);
+      // Loop num number of times.
+      for (let i = 0; i < num; i++) {
+        // Pick a random part.
+        const part = Math.floor(Math.random() * numParts);
+        // Increment that part.
+        parts[part]++;
+      }
+
+      return parts;;
+    }
+
     const command = {
-      'examine': async () => {
+      'create': async () => {
         // Defer the reply, just in case.
         await interaction.deferReply({ ephemeral: true });
 
-        const avatarUrl = interaction.user.avatarURL({ format: 'png', dynamic: true, size: 1024 });
-        
-        const characterDocs = await getFirestore().collection('discord_characters').where('player', '==', interaction.member.id).get();
-        const character = characterDocs.docs[0].data();
-        console.log(`found ${characterDocs.size} character`);
+        const userUid = interaction.member.id;
+        const characterDocs = await getFirestore().collection('discord_characters').where('player', '==', interaction.user.id).get();
+        if (!characterDocs.empty) {
+          return interaction.editReply(`You already have a character. Use \`/character examine\` to see them.`);
+        }
 
-        const embed = new MessageEmbed()
-          .setColor('0x000000')
-          .setTitle(character.displayName)
-          .setImage(avatarUrl);
+        const ref = characterDocs.size > 0 ? characterDocs.docs[0].ref : getFirestore().collection('discord_characters').doc();
 
-        const fields = character.stats.map((stat, index) => {
-          console.log('stat', stat);
-          return {
-            name: stat.displayName,
-            value: getBar(stat.value, stat.max)
-          };
+        // Create the character.
+        const character = new Character({
+          uid: ref.id,
+          displayName: interaction.user.username,
+          player: interaction.user.id,
+          statPoints: 0
         });
 
-        fields.push({ name: 'Available points', value: getBar(character.statPoints, character.statPoints, point) });
-        embed.setFields(fields);
+        // Assign random stats.
+        const numPoints = 21;
+        const parts = split(21, 7);
+        character.stats.forEach((stat, index) => { stat.value = parts[index]; });
+        
+        // Save.
+        await ref.set(character, { merge: true });
+        
+        // Display.
+        await showCharacter(interaction);
+      },
+      'examine': async () => {
+        await interaction.deferReply({ ephemeral: true });
+        await showCharacter(interaction);
+        
 
-        interaction.editReply({ embeds: [embed] });
+        // Wrap an array to a two-dimensional array with specified width.
+        // const map = (array, length) => {
+        //   const result = [];
+        //   for (let i = 0; i < length; i++) {
+        //     result.push(array.slice(i * length, (i + 1) * length));
+        //   }
+        //   return result;
+        // };
+
+        // const row = new MessageActionRow();
+        // character.stats.forEach(stat => {
+        //   row.addComponents(
+        //     new MessageButton()
+        //       .setCustomId(stat.displayName.toLowerCase())
+        //       .setLabel(stat.displayName)
+        //       .setStyle('SECONDARY'))
+        // });
+
+        // const components = [
+        //   new MessageActionRow()
+        //     .addComponents(
+        //       new MessageButton()
+        //         .setCustomId('strength')
+        //         .setLabel('Strength')
+        //         .setStyle('SECONDARY'))
+        //     .addComponents(
+        //       new MessageButton()
+        //         .setCustomId('perception')
+        //         .setLabel('Perception')
+        //         .setStyle('SECONDARY'))
+        //     .addComponents(
+        //       new MessageButton()
+        //         .setCustomId('endurance')
+        //         .setLabel('Endurance')
+        //         .setStyle('SECONDARY'))
+        //     .addComponents(
+        //       new MessageButton()
+        //         .setCustomId('charisma')
+        //         .setLabel('Charisma')
+        //         .setStyle('SECONDARY'))
+        //     .addComponents(
+        //       new MessageButton()
+        //         .setCustomId('intelligence')
+        //         .setLabel('Intelligence')
+        //         .setStyle('SECONDARY')),
+        //   new MessageActionRow()
+        //     .addComponents(
+        //       new MessageButton()
+        //         .setCustomId('agility')
+        //         .setLabel('Agility')
+        //         .setStyle('SECONDARY'))
+        //     .addComponents(
+        //       new MessageButton()
+        //         .setCustomId('luck')
+        //         .setLabel('Luck')
+        //         .setStyle('SECONDARY'))
+        // ];
+        // const message = await interaction.editReply({ embeds: [embed], components });
+        
+
+        // Now add reactions.
+        // await message.react('🅰');
+      },
+      'show': async () => {
+        // Check first.
+        const characterDocs = await getFirestore().collection('discord_characters').where('player', '==', interaction.user.id).get();
+        if (characterDocs.empty) {
+          return interaction.reply(`You don't have a character. Use \`/character create\` to create one.`);
+        }
+
+        await interaction.deferReply({ ephemeral: false });
+        await showCharacter(interaction, { ephemeral: false, doc: characterDocs.docs[0] }); // TODO Figure out how to not do this
+      },
+      'kill': async () => {
+        await interaction.deferReply({ ephemeral: true });
+        // Remove from firebase.
+        const characterDocs = await getFirestore().collection('discord_characters').where('player', '==', interaction.user.id).get();
+        if (characterDocs.empty) {
+          await interaction.editReply({ content: 'You have no character to kill. Use `/character create` to create one.' });
+        } else {
+          await characterDocs.docs[0].ref.delete();
+          await interaction.editReply({ content: 'You killed your character :(' });
+        }
       }
     }[interaction.options.getSubcommand()];
 
